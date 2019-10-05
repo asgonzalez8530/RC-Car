cf26e1563aeeb28b6b7e41ff884c90763f811fbe
Anastasia Gonzalez <gonzalez8530asg@gmail.com>
2019-10-05 14:13:12 -0600

    Add project files.

---
+++ RC Car Client Side/RC Car Client Side/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RC_Car_Client_Side.Models;
+
+namespace RC_Car_Client_Side.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
